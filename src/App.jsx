cfbe33759bdd994b8adf434e082e32e6be12b9dc
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-27 13:05:16 +0530

    initial state as a function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Counter from './Counter'
+import Dumbo from './Dumbo'
 
 function App() {
   return (
     <>
       <h1>State Demo</h1>
-      <Counter />
+      {/* <Counter /> */}
+      <Dumbo />
     </>
   )
 }
---
+++ src/Dumbo.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function generateGameBoard () {
+    console.log("Making a Game Board");
+    return Array(5000);
+}
+
+export default function Dumbo () {
+    const [board, setBoard] = useState(generateGameBoard);
+    return <button onClick={() => setBoard("hello!")}>Click me to change state</button>
+}
