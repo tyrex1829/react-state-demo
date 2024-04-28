82601b503ca506dd5383be8d6192f26ae6c6f032
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-28 09:14:56 +0530

    uuid

--- src/EmojiClicker.jsx
+++ src/EmojiClicker.jsx
@@ -1,16 +1,17 @@
+import { v4 as uuid } from 'uuid';
 import { useState } from "react";
 
 export default function EmojiClicker () {
-    const [emojis, setEmojis] = useState(["😀"])
+    const [emojis, setEmojis] = useState([{id: uuid(), emoji: "😀" }])
     
     const addEmoji = () => {
-        setEmojis(oldEmojis => [...oldEmojis, "😂"]);
+        setEmojis(oldEmojis => [...oldEmojis, {id: uuid(), emoji: "😂" }]);
     };
 
     return (
         <div>
             {emojis.map(e => (
-                <span style={{ fontSize: "4rem" }}>{e}</span>
+                <span key={e.id} style={{ fontSize: "4rem" }}>{e.emoji}</span>
             ))}
             <button onClick={addEmoji}>Add Emoji</button>
         </div>
