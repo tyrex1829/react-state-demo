4c465518d0efb7004fa8d0b1ab2a1f75a0aa8e1d
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-28 09:56:32 +0530

    button to update all

--- src/EmojiClicker.jsx
+++ src/EmojiClicker.jsx
@@ -14,12 +14,21 @@ export default function EmojiClicker () {
         })
     }
 
+    const makeEverythingHeart = () => {
+        setEmojis(prevEmojis => {
+            return prevEmojis.map(e => {
+                return {...e, emoji: "💓"}
+            })
+        })
+    }
+
     return (
         <div>
             {emojis.map(e => (
                 <span onClick={() => deleteEmoji(e.id)} key={e.id} style={{ fontSize: "4rem", cursor: "pointer" }}>{e.emoji}</span>
             ))}
             <button onClick={addEmoji}>Add Emoji</button>
+            <button onClick={makeEverythingHeart}>Make them all hearts</button>
         </div>
     );
 }
